87461dc79d26869394501ab3f1630613ec522924
Nischal Shakya <nischal.shk@gmail.com>
2020-01-09 11:37:26 +0545

    Added server response models for image and signup

--- app/src/main/java/com/aboveall/twitterclone/serverresponse/ImageResponse.java
+++ app/src/main/java/com/aboveall/twitterclone/serverresponse/ImageResponse.java
@@ -1,4 +1,18 @@
 package com.aboveall.twitterclone.serverresponse;
 
 public class ImageResponse {
+
+    private String filename;
+
+    public ImageResponse(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 }
