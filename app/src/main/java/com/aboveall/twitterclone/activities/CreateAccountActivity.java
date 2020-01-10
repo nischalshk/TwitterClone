d68c3251585da74f5548341f9166ebbb4814bc32
Nischal Shakya <nischal.shk@gmail.com>
2020-01-10 09:47:57 +0545

    First create account page binded

---
+++ app/src/main/java/com/aboveall/twitterclone/activities/CreateAccountActivity.java
@@ -0,0 +1,67 @@
+package com.aboveall.twitterclone.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aboveall.twitterclone.R;
+
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private EditText edtName, edtEmailorPhone;
+    private TextView txtEmailorPhone;
+    private Button btnNextCAA;
+    private ImageButton btnImageBackCCA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        edtName = findViewById(R.id.edtName);
+        edtEmailorPhone = findViewById(R.id.edtPhoneOrEmail);
+        txtEmailorPhone = findViewById(R.id.btnPhoneOrEmail);
+        btnNextCAA = findViewById(R.id.btnNextCAA);
+        btnImageBackCCA = findViewById(R.id.imagebtnbackCAA);
+
+        edtEmailorPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtEmailorPhone.setText("Use phone instead");
+                txtEmailorPhone.setVisibility(View.VISIBLE);
+            }
+        });
+        btnNextCAA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                String email = edtEmailorPhone.getText().toString();
+
+                if (!name.equals("")&&!email.equals("")){
+                    btnNextCAA.setClickable(false);
+                }
+                else{
+                    btnNextCAA.setClickable(true);
+                    Intent intent = new Intent(CreateAccountActivity.this, PolicyActivity.class);
+                    intent.putExtra("Name", name);
+                    intent.putExtra("Email",email);
+                    startActivity(intent);
+                }
+            }
+        });
+        btnImageBackCCA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateAccountActivity.this, LoginOrSignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
